9fa368c98fb15f521e82cc10ff2bd6eed264730d
weskerliu <weskerliu88@gmail.com>
2020-07-24 00:18:31 -0400

    admin basic page

---
+++ SESTOPIA/frontend/src/Components/Tables/DataTable.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap';
+import ModalForm from '../Modals/Modal'
+
+class DataTable extends Component {
+
+  deleteItem = id => {
+    let confirmDelete = window.confirm('Delete item forever?')
+    if(confirmDelete){
+      fetch('http://localhost:6011/api/v1/tools/', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        this.props.deleteItemFromState(id)
+      })
+      .catch(err => console.log(err))
+    }
+
+  }
+
+  render() {
+
+    const items = this.props.items.map(item => {
+      return (
+        <tr key={item.id}>
+          <th scope="row">{item.id}</th>
+          <td>{item.name}</td>
+          <td>{item.descrption}</td>
+          <td>
+            <div style={{width:"110px"}}>
+              <ModalForm buttonLabel="Edit" item={item} updateState={this.props.updateState}/>
+              {' '}
+              <Button color="danger" onClick={() => this.deleteItem(item.id)}>Del</Button>
+            </div>
+          </td>
+        </tr>
+        )
+      })
+
+    return (
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>name</th>
+            <th>descrption</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default DataTable
