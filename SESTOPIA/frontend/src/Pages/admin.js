fef2430909d304e5940ea2c54bcdd672076af788
weskerliu <weskerliu88@gmail.com>
2020-07-25 00:27:23 -0400

    dix

--- SESTOPIA/frontend/src/Pages/admin.js
+++ SESTOPIA/frontend/src/Pages/admin.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
-import ModalForm from './../Components/Modals/Modal'
-import DataTable from './../Components/Tables/DataTable'
+import ModalForm from '../Components/Modals/Modal'
+import DataTable from '../Components/Tables/DataTable'
 import { CSVLink } from "react-csv"
 
 class Admin extends Component {
